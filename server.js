7af7541ffdaa6f2ebad730f0441eabf23cb3a2f2
shpokas <s.shpokas@gmail.com>
2024-10-23 13:11:51 +0300

    defined api end point structure

--- server.js
+++ server.js
@@ -8,3 +8,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+//Defining structure
+app.use("/api/shoes", require("./routes/shoeRoutes"));
+app.use("/api/runs", require("./routes/runRoutes"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
