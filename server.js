60bad3b9ef90b733cdc5aa405ff50249a9a888ed
shpokas <s.shpokas@gmail.com>
2024-10-23 13:09:34 +0300

    Initial variable setup , with environment variables

---
+++ server.js
@@ -0,0 +1,10 @@
+require("dotenv").config(); //Load enviroment variables
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
