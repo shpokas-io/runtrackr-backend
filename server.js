cd9c8fa047420f910d43ba4e2fd0aa202014a307
shpokas <s.shpokas@gmail.com>
2024-10-23 15:42:42 +0300

    Added strava auth logic

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
 require("dotenv").config(); //Load enviroment variables
 
+const axios = require("axios");
+const querystring = require("querystring");
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+// Strava Auth Callback Route
+app.get("/auth/strava/callback", async (req, res) => {
+  const authorizationCode = req.query.code;
 
-//Defining structure
-app.use("/api/shoes", require("./routes/shoeRoutes"));
-app.use("/api/runs", require("./routes/runRoutes"));
+  try {
+    //Exchange the authorization code for an access token
+    const response = await axios.post(
+      "https://www.strava.com/oauth/token",
+      querystring.stringify({
+        client_id: process.env.STRAVA_CLIENT_ID,
+        client_secret: process.env.STRAVA_CLIENT_SECRET,
+        code: authorizationCode,
+        grant_type: "authorization_code",
+      })
+    );
+    //Log the access token to the console
+    console.log("Access Token:", accessToken);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    //send a response back to the client
+    res.send("Authorization successful! You now have an access token");
+  } catch (error) {
+    console.error("Error exchanging code for token:", error);
+    res.status(500).send("Error during token exchange");
+  }
+});
+
+//Run the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
